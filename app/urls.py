60a4a7796e1e1e4ed6bfb7ce7a1ec5f04d5db7fd
alafaethy <alafaethy@gmail.com>
2024-04-17 12:45:58 -0300

    teste6

--- app/urls.py
+++ app/urls.py
@@ -24,5 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api_view.urls'))
-] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
